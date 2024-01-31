10ee60ac6c7031d097f2288fe771846f6820cfa1
Eric Zhang <ericbkzhang@gmail.com>
2024-01-31 16:32:18 -0500

    Created a toErrorType() method to convert a String to an ErrorType enum

--- src/Error.java
+++ src/Error.java
@@ -4,30 +4,72 @@ public class Error {
         LINKING,
         RUNTIME,
         UNDEFINED;
+
+        /**
+         * Attempts to convert a String to an ErrorType
+         * If it it does not match any ErrorType, default to UNDEFINED
+         * 
+         * @param errorStr a String that contains an ErrorType
+         */
+        private static ErrorType toErrorType(String errorStr) {
+            // try to convert the String to the matching ErrorType and store it
+            try {
+                error = ErrorType.valueOf(errorStr.trim().toUpperCase());
+
+            } catch (IllegalArgumentException | NullPointerException e) { // if it fails
+                // set it to the default(i.e. UNDEFINED)
+                error = ErrorType.UNDEFINED;
+            }
+        }
     }
 
     private ErrorType error;
-    private int minutesToFixWithoutHelp;
+
+    // how many minutes it takes for the student to fix the error
+    // if they receive help from the teacher
     private int minutesToFixWithHelp;
 
+    // how many minutes it takes for the student to fix the error
+    // if they do not receive help from the teacher
+    private int minutesToFixWithoutHelp;
+
     public Error() {
-        error = ErrorType.UNDEFINED;
+        this(ErrorType.UNDEFINED, 1, 1);
     }
 
     public Error(ErrorType error) {
-        this.error = error;
+        this(Error, 1, 1);
     }
 
     /**
-     * Take in a String and save the corresponding ErrorType
+     * Take in a String and convert it to the corresponding ErrorType
      * Defaults to UNDEFINED
      * 
      * @param errorStr a String that holds the error
      */
     public Error(String errorStr) {
-        setError(errorStr);
+        this(ErrorType.toErrorType(errorStr), 1, 1);
     }
 
+    public Error(ErrorType error, int minutesToFixWithHelp, int minutesToFixWithoutHelp) {
+        this.error = error;
+        this.minutesToFixWithHelp = minutesToFixWithHelp;
+        this.minutesToFixWithoutHelp = minutesToFixWithoutHelp;
+    }
+
+    /**
+     * Take in a String and convert it to the corresponding ErrorType
+     * Defaults to UNDEFINED
+     * 
+     * @param errorStr                a String that holds the error
+     * @param minutesToFixWithHelp    how long it takes for the student to fix the error 
+     *                                if they receive help from the teacher
+     * @param minutesToFixWithoutHelp how long it takes for the student to fix the error 
+     *                                if they do not receive help from the teacher
+     */
+    public Error(String errorStr, int minutesToFixWithHelp, int minutesToFixWithoutHelp) {
+        this(ErrorType.toErrorType(errorStr), minutesToFixWithHelp, minutesToFixWithoutHelp);
+    }
 
     public ErrorType getError() {
         return error;
@@ -44,13 +86,6 @@ public class Error {
      * @param errorStr a String that holds the demeanor
      */
     private void setError(String errorStr) {
-        // try to convert the String to the matching ErrorType and store it
-        try {
-            error = ErrorType.valueOf(errorStr.trim().toUpperCase());
-
-        } catch (IllegalArgumentException | NullPointerException e) { // if it fails
-            // set it to the default(i.e. UNDEFINED)
-            error = ErrorType.UNDEFINED;
-        }
+        this.error = ErrorType.toErrorType(errorStr);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         Teacher teacher = new Teacher("Eric Zhang", new Experience("FIRST_YEAR"));
+
+        Error error = new Error("compiling", 3, 15);
+        Student student = new Student();
     }
 }
